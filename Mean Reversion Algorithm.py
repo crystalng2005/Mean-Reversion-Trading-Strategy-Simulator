1605660c933e9681076ae5510fe97a62f24212eb
slappinwack <crystalngshulu9909@gmail.com>
2025-07-28 00:52:09 +0800

    testingggg

--- Mean Reversion Algorithm.py
+++ Mean Reversion Algorithm.py
@@ -1,1 +1,15 @@
-print("I AM THINKING")
+'''
+Mean Reversion Strategies:
+
+1. Statistical Analysis
+- Z-scores to meausre asset price deviation from its mean
+- Z-score above 1.5 or below -1.5
+
+2. Pairs Trading
+- Identify two correlated assets
+
+3. Volatility
+-
+
+
+'''
