4c0ff9aeedacc1593e03fcc855de2d637aa58df2
cssimpson <smpsnchen@gmail.com>
2025-07-28 02:55:48 +1000

    Data.py

--- Data.py
+++ Data.py
@@ -1,5 +1,26 @@
+# yfinance for stock data
 import yfinance as yf
+import matplotlib.pyplot as plt
+import sklearn as sk
+import pandas as pd
+import datetime
 
-# download data
-# stock name start date and end date(XXXX-XX-XX)
-data = yf.download('AAPL', start='2023-01-01', end='2023-12-31')
+# download historical data for stock
+# stock name/start date/end date(XXXX-XX-XX)
+stocks = 'AAPL'
+data = yf.download(stocks, start='2023-01-01', end='2023-12-31')
+
+# This show the first few rows of data FYI
+# print(data.head())
+
+# Visualization with matplotlib
+plt.figure(figsize=(69, 42))
+plt.plot(data['Close'], label='Closing Price')
+plt.title(f"{stocks} Closing Price Over Time")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.legend()
+plt.grid()
+plt.show()
+
+# Machine Learning with sklearn
